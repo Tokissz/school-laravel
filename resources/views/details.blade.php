7807fcdf6deba9e14d0cc517ba8b9c9bae4fcecd
Tokissz <161070688+Tokissz@users.noreply.github.com>
2024-05-17 14:18:06 +0700

    Feat : date time at detail page

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,24 +1,14 @@
 @include('header')
 @include('navbar')
-<!-- <div id="boxes">
-    <div style="top: 50%; left: 50%; display: none;" id="dialog" class="window">
-        <div id="san">
-            <a href="#" class="close agree"><img src="{{ asset('assets/img/icons/close-icon.png') }} " width="25" style="float:right; margin-right: -25px; margin-top: -20px;"></a>
-            <img class="responsive" src="{{ asset('assets/img/popup/pop-up.jpg') }} ">
-        </div>
-    </div>
-    <div style="width: 100%; font-size: 32pt; color:white; height: auto; display: none; opacity: 0.4;" id="mask">
-    </div>
-</div> -->
-
-
-<!-- ======= Hero Section ======= -->
+<!-- ======= banner Section ======= -->
 @include ('banner')
+<!-- =======  end banner Section ======= -->
 <div class="container-fluid">
     <div class="container p-4">
         <br>
         <div class="text-center">
             <h3 class="text-center">{{ $post->postTitle }}</h3>
+            <div class="d-flex justify-content-end">{{ \Carbon\Carbon::parse($post->created_at)->locale('th')->isoFormat('LL LT') }} น.</div>
             <hr>
         </div>
         <div class="min-vh-100 w-auto d-inline-block">
